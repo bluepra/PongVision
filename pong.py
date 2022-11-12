51eebbe3ab05f21e14100e2964d273dee26c1e34
Ryan Almizyed <almizyed@wisc.edu>
2022-11-12 13:01:04 -0600

    Main game loop

---
+++ pong.py
@@ -0,0 +1,44 @@
+# Import the pygame library and initialise the game engine
+import pygame
+pygame.init()
+
+# Define some colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# Open a new window
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Pong")
+
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
+carryOn = True
+ 
+# The clock will be used to control how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+              carryOn = False # Flag that we are done so we exit this loop
+ 
+    # --- Game logic should go here
+ 
+
+ 
+    # --- Drawing code should go here
+    # First, clear the screen to black. 
+    screen.fill(BLACK)
+    #Draw the net
+    pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+     
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+#Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
