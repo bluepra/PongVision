670e73160f185c5e5e0ffb722c46df48e4e02062
Prannav Arora <parora9@wisc.edu>
2022-11-12 16:37:19 -0600

    VideoProcessor provides interface to get Y-coord

---
+++ VideoProcessor.py
@@ -0,0 +1,111 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import copy
+import numpy as np
+
+class VideoProcessor():
+
+    def __init__(self) -> None:
+        # Camer
+        self.cam = cv.VideoCapture(0)
+
+        # Pose detection
+        self.mp_pose = mp.solutions.pose
+        self.mp_draw = mp.solutions.drawing_utils
+        self.pose = self.mp_pose.Pose()
+
+        # FPS Information
+        cur_time = None
+        prev_time = 0   
+
+    
+    def show_fps(self, frame):
+        self.cur_time = time.time()
+        fps = int(1/(self.cur_time - self.prev_time))
+        prev_time = self.cur_time
+        return cv.putText(frame, 'FPS:' + str(fps), (70,80), cv.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
+
+    def close(self):
+        self.cam.release()
+        cv.destroyAllWindows()
+
+    def split_frame_into_2(self, frame):
+        height, width, channels = frame.shape
+
+        # Right Half - left side is blacked out
+        r = copy.deepcopy(frame)
+        r[:, 0:width//2] = np.zeros(shape=(height, width//2, channels))
+
+        # Left Half - right side is blacked out
+        frame[:, width//2:] = np.zeros(shape=(height, width//2, channels))
+        
+        return frame, r
+    
+    def stitch_left_and_right(self, left, right):
+        h,w,c = left.shape
+        left[:, w//2:] = right[:, w//2:]
+        cv.line(left, pt1=(w//2,0), pt2=(w//2,h), color=(255,255,255), thickness=100)
+        return left
+    
+    def draw_nose_line(self, frame, coord, side):
+        if coord:
+            x,y = coord
+            if side == 'left':
+                color = (255,0,0)
+            else:
+                color = (0,0,255)
+            frame = cv.line(frame, pt1 = (x-20, y), pt2=(x+20, y), color=color, thickness=4)
+        return frame
+    
+    def get_nose_coord(self, frame):
+        frame_width = frame.shape[1]
+        frame_height = frame.shape[0]
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        # Get the Pose Landmarks
+        results = self.pose.process(frame)
+        if results.pose_landmarks:
+            self.mp_draw.draw_landmarks(frame, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+
+            # Nose Y is the Y coordinate of your nose in the frame
+            nose_y = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.NOSE].y * frame_height)
+            nose_x = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.NOSE].x * frame_width)
+        else:
+            return None
+
+        return (nose_x, nose_y)
+
+
+
+if __name__ == '__main__':
+    vp = VideoProcessor()
+
+    while True:
+        ret, frame = vp.cam.read()
+        
+        if not ret: continue
+
+        h,w,c = frame.shape
+        frame = cv.flip(frame, 1)
+
+        # frame = show_fps(frame)
+
+        # Split frame into 2 halves
+        left, right = vp.split_frame_into_2(frame)
+
+        # # Get poses for both halves
+        left_nose_coord = vp.get_nose_coord(left)
+        right_nose_coord = vp.get_nose_coord(right)
+
+        left = vp.draw_nose_line(left, left_nose_coord, 'left')
+        right = vp.draw_nose_line(right, right_nose_coord, 'right')
+
+        frame = vp.stitch_left_and_right(left, right)
+
+        cv.imshow("Video Feed", frame)
+
+        if cv.waitKey(33) == ord('q'):
+            break
+
+    vp.close()
