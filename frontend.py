95d320702adb838f4ff2614f3f85db5f44ff1b67
MitchBrenner <mkbrenner3@wisc.edu>
2022-11-12 15:06:51 -0600

    added methods class

--- frontend.py
+++ frontend.py
@@ -3,6 +3,8 @@ import pygame
 from paddle import Paddle
 from ball import Ball
 import cv2 as cv
+import time
+import methods
  
 pygame.init()
 
@@ -12,10 +14,35 @@ WIDTH, HEIGHT = 900, 500
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
-# start webcam
-cap = cv.VideoCapture(0)
-cap.set(3, WIDTH)  # Width
-cap.set(4, HEIGHT)  # Height
+# set up camera
+cam = methods.cam_set_up(WIDTH, HEIGHT)
+face_cascade=cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_alt2.xml")
+ds_factor=0.6
+
+def face_detection(input_frame):
+    frame = cv.resize(input_frame,None,fx=ds_factor,fy=ds_factor, interpolation=cv.INTER_AREA)
+    
+    # Face dectection
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    face_rects = face_cascade.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in face_rects:
+        # cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        paddleA.rect.y = (y+h)/2
+        break
+    return frame
+
+
+
+    # gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    # face_rects = face_cascade.detectMultiScale(gray,1.3,5)
+    
+    
+    # frame = cv.resize(frame,None,fx=ds_factor,fy=ds_factor, interpolation=cv.INTER_AREA) 
+    # for (x,y,w,h) in face_rects:
+    #     # cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    #     paddleA.rect.y = (y+h)/2
+    #     break
+    
  
 # Open a new window
 size = (WIDTH, HEIGHT)
@@ -51,6 +78,19 @@ clock = pygame.time.Clock()
 #Initialise player scores
 scoreA = 0
 scoreB = 0
+
+cur_time = None
+prev_time = 0
+def show_fps(frame):
+    global cur_time
+    global prev_time
+
+    cur_time = time.time()
+    fps = int(1/(cur_time-prev_time))
+    prev_time = cur_time
+    
+    # print('FPS:', fps)
+    return cv.putText(frame, str(fps), (70,80), cv.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
  
 # -------- Main Program Loop -----------
 while carryOn:
@@ -62,19 +102,19 @@ while carryOn:
                 if event.key==pygame.K_x: #Pressing the x Key will quit the game
                      carryOn=False
  
-    #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B) 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        paddleA.moveUp(5)
-    if keys[pygame.K_s]:
-        paddleA.moveDown(5)
-    if keys[pygame.K_UP]:
-        paddleB.moveUp(5)
-    if keys[pygame.K_DOWN]:
-        paddleB.moveDown(5)    
+
  
     # --- Game logic should go here
     all_sprites_list.update()
+
+    # face track
+    ret, frame = cam.read()
+    
+    frame, nose_y = methods.pose_estimation(frame)
+    paddleA.rect.y = nose_y - 100
+    paddleB.rect.y = nose_y - 100
+
+   
     
     #Check if the ball is bouncing against any of the 4 walls:
     if ball.rect.x>=WIDTH:
@@ -113,6 +153,14 @@ while carryOn:
      
     # --- Limit to 60 frames per second
     clock.tick(60)
+
+    frame = show_fps(frame)
+    if ret:
+        cv.imshow("Hello", frame)
+        
+
+        if cv.waitKey(33) == ord('q'):
+            break
  
 #Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
