65f956d3449198fdd787b5a64a8f4b6bfeb0bf54
Prannav Arora <parora9@wisc.edu>
2022-11-12 16:29:39 -0600

    Trying to get 2 nose coords

--- pose_tracking.py
+++ pose_tracking.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import mediapipe as mp
 import time
 import copy
+import numpy as np
 
 
 cam = cv.VideoCapture(0)
@@ -16,7 +17,7 @@ def show_fps(frame):
     global prev_time
 
     cur_time = time.time()
-    fps = int(1/(cur_time-prev_time))
+    fps = int(1/(cur_time - prev_time))
     prev_time = cur_time
     
     # print('FPS:', fps)
@@ -35,41 +36,79 @@ def face_detection(input_frame):
         break
     return frame
 
-
 def split_frame_into_2(frame):
-    height, width = frame.shape
-    l = copy.deepcopy(frame)
-    r = frame
-    return
+    height, width, channels = frame.shape
 
+    # Right Half - left side is blacked out
+    r = copy.deepcopy(frame)
+    r[:, 0:width//2] = np.zeros(shape=(height, width//2, channels))
 
-def pose_estimation(frame):
+    # Left Half - right side is blacked out
+    frame[:, width//2:] = np.zeros(shape=(height, width//2, channels))
     
+    return frame, r
+
+def get_nose_coord(frame):
+    frame_width = frame.shape[1]
+    frame_height = frame.shape[0]
     frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    # Get the Pose Landmarks
     results = pose.process(frame)
     if results.pose_landmarks:
         mp_draw.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-        frame_width = frame.shape[1]
-        frame_height = frame.shape[0]
-        print('Frame Height', frame_height)
-        print('Frame Width', frame_width)
+        # Nose Y is the Y coordinate of your nose in the frame
         nose_y = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * frame_height)
-        frame = cv.putText(frame, 'Nose: ' + str(nose_y), (70,130), cv.FONT_HERSHEY_PLAIN, 3, (0,255,0), 3)
-        # for i, pt in enumerate(results.pose_landmarks.landmark):
-        #     print(i, pt)
-        frame = cv.line(frame,pt1 = (0, nose_y), pt2=(frame_width, nose_y), color=(255,0,0), thickness=4)
+        nose_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * frame_width)
+    else:
+        return None
 
-    return cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+    return (nose_x, nose_y)
 
+def draw_nose_line(frame, coord, side):
+    if coord:
+        x,y = coord
+        if side == 'left':
+            color = (255,0,0)
+        else:
+            color = (0,0,255)
+        frame = cv.line(frame, pt1 = (x-20, y), pt2=(x+20, y), color=color, thickness=4)
+    return frame
+
+def stitch_left_and_right(left, right):
+    h,w,c = left.shape
+    left[:, w//2:] = right[:, w//2:]
+    cv.line(left, pt1=(w//2,0), pt2=(w//2,h), color=(255,255,255), thickness=100)
+    return left
 
 while True:
     ret, frame = cam.read()
-    
-    # frame = face_detection(frame)   
+    if not ret: continue
 
-    frame = show_fps(frame)
-    frame = pose_estimation(frame)
+    h,w,c = frame.shape
+    frame = cv.flip(frame, 1)
+
+    # frame = show_fps(frame)
+
+    # Split frame into 2 halves
+    left, right = split_frame_into_2(frame)
+
+    # # Get poses for both halves
+    left_nose_coord = get_nose_coord(left)
+    right_nose_coord = get_nose_coord(right)
+
+    # if left_nose_coord:
+    #     print('Left Nose', left_nose_coord[1], ' ------ ', end='')
+    # if right_nose_coord:
+    #     print('Right Nose', right_nose_coord[1], end='')
+    # print()
+
+
+    left = draw_nose_line(left, left_nose_coord, 'left')
+    right = draw_nose_line(right, right_nose_coord, 'right')
+
+    frame = stitch_left_and_right(left, right)
 
     cv.imshow("Video Feed", frame)
 
