dc423377ecbcd47ab3207d30bd33359246d53c71
Ryan Almizyed <almizyed@wisc.edu>
2022-11-12 13:02:24 -0600

    Add paddles

--- pong.py
+++ pong.py
@@ -1,5 +1,7 @@
 # Import the pygame library and initialise the game engine
 import pygame
+from paddle import Paddle
+
 pygame.init()
 
 # Define some colors
@@ -10,6 +12,21 @@ WHITE = (255,255,255)
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
+
+paddleA = Paddle(WHITE, 10, 100)
+paddleA.rect.x = 20
+paddleA.rect.y = 200
+
+paddleB = Paddle(WHITE, 10, 100)
+paddleB.rect.x = 670
+paddleB.rect.y = 200
+
+#This will be a list that will contain all the sprites we intend to use in our game.
+all_sprites_list = pygame.sprite.Group()
+
+# Add the paddles to the list of sprites
+all_sprites_list.add(paddleA)
+all_sprites_list.add(paddleB)
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 carryOn = True
@@ -23,16 +40,22 @@ while carryOn:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
               carryOn = False # Flag that we are done so we exit this loop
+        elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_x: #Pressing the x Key will quit the game
+                     carryOn=False  
  
     # --- Game logic should go here
+    all_sprites_list.update()
  
-
  
     # --- Drawing code should go here
     # First, clear the screen to black. 
     screen.fill(BLACK)
     #Draw the net
     pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
+    
+    #Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
+    all_sprites_list.draw(screen) 
  
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
