c357f0ed89ce2114611ecf7d95ac4309b6f20e08
bluepra <parora9@wisc.edu>
2022-11-13 03:09:00 -0600

    Game Works now

--- util.py
+++ util.py
@@ -88,7 +88,8 @@ class Ball(pygame.sprite.Sprite):
         # Draw the ball (a rectangle!)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         
-        self.velocity = [randint(4,8), randint(-8,8)]
+        # self.velocity = [randint(4,8), randint(-8,8)]
+        self.velocity = [0,0]
         
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
