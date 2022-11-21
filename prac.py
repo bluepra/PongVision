4aa938a5ef5a6d16fe67bad816d45d0d0b218c08
bluepra <parora9@wisc.edu>
2022-11-20 18:40:50 -0600

    starting to refactory ball class code

--- prac.py
+++ prac.py
@@ -1,22 +1,53 @@
 import pygame
 from time import time, sleep
+import sys
 
 pygame.init()
 
-start_time = time()
+screen = pygame.display.set_mode((1000,700))
+
+class Ball:
+    def __init__(self) -> None:
+        self.image = pygame.image.load('/Users/prannav/Coding/CheeseHacks/CheeseHacks_2022/tennis_ball.jpeg')
+        
+        self.rect = self.image.get_rect()
+        print('image width and height', self.rect.width, self.rect.height)
+        self.rect.topleft = (100,100)
+
+    def update(self, mouse_pos):
+        print(self.rect.topleft)
+        
+    
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+done = False
+ball = Ball()
 
 clock = pygame.time.Clock()
 
-count = 0
-while True:
-    cur_time = time()
-    # print(cur_time - start_time)
-    # sleep(3)
-    count +=1
-    if (cur_time - start_time) >= 1:
-        break
+def quit_handler():
+    pygame.quit()
+    sys.exit()
 
-    
+while not done:
+    game_surface = pygame.Surface((1000,700))
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                quit_handler()
+
+    ball.update(pygame.mouse.get_pos())
+    ball.draw(game_surface)
+
+    screen.blit(game_surface, (0,0))
+    pygame.display.update()
+
     clock.tick(60)
 
-print(f'We looped {count} timees')
+quit_handler()
+
--- util.py
+++ util.py
@@ -85,18 +85,18 @@ class Ball(pygame.sprite.Sprite):
         # Set the background color and set it to be transparent
         self.width = width
         self.height = height
-        self.image = pygame.Surface([width, height])
-        self.image.fill(BLACK)
-        self.image.set_colorkey(BLACK)
+        self.surface = pygame.Surface([width, height])
+        self.surface.fill(BLACK)
+        self.surface.set_colorkey(BLACK)
  
-        # Draw the ball (a rectangle!)
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        # # Draw the ball (a rectangle!)
+        # pygame.draw.rect(self.surface, color, [0, 0, width, height])
         
         # self.velocity = [randint(4,8), randint(-8,8)]
         self.velocity = [0,0]
         
         # Fetch the rectangle object that has the dimensions of the image.
-        self.rect = self.image.get_rect()
+        self.rect = self.surface.get_rect()
         
     def update(self):
         self.rect.x += self.velocity[0]
@@ -105,7 +105,6 @@ class Ball(pygame.sprite.Sprite):
     def bounce(self):
         self.velocity[0] = -self.velocity[0]*uniform(.9,1.25)
         self.velocity[1] = randint(-8,8)
-
 
 class Paddle(pygame.sprite.Sprite):
     #This class represents a paddle. It derives from the "Sprite" class in Pygame.
